28fc55f3116a130cda54c63df14ed4e3714e2c59
Paul Han <hanpaul@users.noreply.github.com>
2025-10-25 18:09:40 -0700

    feat: Update asset URLs to local paths
    Switches image and background texture URLs from external CDNs to local assets. This improves performance and offline accessibility.

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 
 export const REGISTRATION_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdi9YLjmkowsle5FxYXq63Qjt4aKxUzUyQrJ_uU-bNZJScBFA/viewform";
-export const BANNER_IMAGE_URL = "https://i.imgur.com/PsmJt9g.jpg";
-export const SPEAKER_IMAGE_URL = "https://i.imgur.com/1B9q2e9.jpeg";
-export const BACKGROUND_TEXTURE_URL = "https://i.imgur.com/gK9p2hQ.jpeg";
+export const BANNER_IMAGE_URL = "./assets/banner.jpg";
+export const SPEAKER_IMAGE_URL = "./assets/dr_navarra.png";
+export const BACKGROUND_TEXTURE_URL = "./assets/background.png";
